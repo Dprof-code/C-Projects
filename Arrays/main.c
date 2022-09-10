9ed912f8c13cc4f4d9dc756ec2d26f8e6f6a7a81
Dprof-Code <olawaleade15@gmail.com>
2022-09-09 20:59:09 -0500

    C project files

--- Arrays/main.c
+++ Arrays/main.c
@@ -7,9 +7,7 @@
 
 int main()
 {
-    char likes[10000] = {"Praying", "Coding", "Eating", "Sleeping"};
     int evenNum[] = {2, 4, 6, 8, 10};
-
     printf("My favorite number is %d", evenNum[1]);
 
     return (0);
